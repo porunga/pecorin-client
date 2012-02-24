fd7db08c5125198a5a931ae212e899c87b427302
asktkhr <bbclapton@gmail.com>
2012-02-25 00:27:17 +0900

    Modify get pecorin_token and send intent to DeviceRegistrationActivity

--- src/com/porunga/pecorin/FacebookAuthActivity.java
+++ src/com/porunga/pecorin/FacebookAuthActivity.java
@@ -1,17 +1,32 @@
 package com.porunga.pecorin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 public class FacebookAuthActivity extends Activity {
-
-	private static final String LOGIN_URL = "http://localhost/login";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,17 +34,85 @@ public class FacebookAuthActivity extends Activity {
 		setContentView(R.layout.facebook_auth_activity);
 
 		WebView webView = (WebView) findViewById(R.id.webView);
-		webView.loadUrl(LOGIN_URL);
-	
-		webView.setWebViewClient(new WebViewClient(){
+		webView.loadUrl(getString(R.string.PecorinServerURL) + "/login");
+
+		webView.setWebViewClient(new WebViewClient() {
 			@Override
-			public void onPageStarted(WebView view, String url, Bitmap favicon){
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				setProgressBarIndeterminateVisibility(true);
 			}
-			
-		    @Override
-		    public boolean shouldOverrideUrlLoading(WebView view, String url) {		    	
-		      return super.shouldOverrideUrlLoading(view, url);
-		    }
+
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				Pattern pattern = Pattern.compile("http://.*success.*auth=(.*)");
+				Matcher matcher = pattern.matcher(url);
+				if (matcher.find()) {
+
+					JSONObject json = getUserData(url);
+					
+					String pecorinToken = null;
+					String facebookId = null;
+					Intent intent = new Intent(getApplicationContext(), DeviceRegistrationActivity.class);
+					
+					try {
+						pecorinToken = matcher.group(1);
+						facebookId = json.getString("facebook_id");
+
+						intent.putExtra("pecorin_token", pecorinToken);						
+						intent.putExtra("facebook_id", facebookId);
+						intent.putExtra("name", json.getString("name"));
+						intent.putExtra("image_url", json.getString("image_url"));
+						
+						SharedPreferences preferences = getSharedPreferences("preference", Activity.MODE_PRIVATE);
+						Editor editor = preferences.edit();
+						editor.putString("pecorin_token", pecorinToken);
+						editor.putString("facebook_id", facebookId);
+						editor.commit();
+						
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					startActivity(intent);
+				}
+				return super.shouldOverrideUrlLoading(view, url);
+			}
 		});
+	}
+	
+	private JSONObject getUserData(String url){
+		HttpClient client = new DefaultHttpClient();
+		
+		HttpGet request = new HttpGet(url);
+		StringBuilder builder = new StringBuilder();
+
+		try {
+			HttpResponse response = client.execute(request);
+			int statusCode = response.getStatusLine().getStatusCode();
+			if (statusCode == HttpURLConnection.HTTP_OK) {
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
+			}
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		JSONObject json = null;
+		try {
+			json = new JSONObject(builder.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return json;
 	}
 }
